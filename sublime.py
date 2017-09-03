66d891fdf3a4c5fdebe271acd03cdcbb5fceddaf
Giovanni Porta <gporta@outlook.com>
2017-09-02 23:39:40 -0700

    Custom completions formatting for Sublime Text

--- sublime.py
+++ sublime.py
@@ -1,4 +1,5 @@
 import json
+import re
 from util import format_args
 
 scope = "source.lua"
@@ -20,4 +21,19 @@ def gen_file():
         "completions": completions
     }
 
-    return json.dumps(final_data, indent=4)
+    pre_format_json = json.dumps(final_data, indent=4)
+    formatted_json = perform_formatting(pre_format_json)
+
+    return formatted_json
+
+def perform_formatting(json):
+    completions_regex = re.compile(r'\"completions\":\s+\[')
+    trigger_regex = re.compile(r'{\s+\"trigger\":\s\"(.+)\",\s+\"contents\":.+\s+\"(.+)\"\s+.+\s+}')
+
+    completion_sub = '\n    "completions": \n    ['
+    trigger_sub = '{ "trigger": "\g<1>", "contents": "\g<2>" }'
+
+    new_file = completions_regex.sub(completion_sub, json)
+    new_file = trigger_regex.sub(trigger_sub, new_file)
+
+    return new_file
